05cc4858d5ad05ceaaacced08c74f181122b357b
Clarisse-3924 <cuwizeyimana3924@gmail.com>
2020-12-10 22:11:18 +0200

    modifying file

--- src/main/resources/public/js/StoreShoes.js
+++ src/main/resources/public/js/StoreShoes.js
@@ -105,3 +105,51 @@ function updateCartTotal() {
     total = Math.round(total * 100) / 100
     document.getElementsByClassName('cart-total-price')[0].innerText =  total + 'Rwf'
 }
+$(document).ready(function () {
+    $('.doing').click(function () {
+         $('.content1').show()
+
+    })
+});
+$(document).ready(function () {
+     $('.mission').click(function () {
+          $('.content1').hide()
+          $('.content2').show()
+
+     })
+ });
+ $(document).ready(function () {
+     $('.goal').click(function () {
+          $('.content2').hide()
+          $('.content3').show()
+
+     })
+ });
+ $(document).ready(function () {
+     $('.doing').click(function () {
+          $('.content3').hide()
+          $('.content1').show()
+
+     })
+ });
+ $(document).ready(function () {
+     $('.mission').click(function () {
+          $('.content3').hide()
+          $('.content2').show()
+
+     })
+ });
+ $(document).ready(function () {
+     $('.doing').click(function () {
+          $('.content2').hide()
+          $('.content1').show()
+
+     })
+ });
+ $(document).ready(function () {
+     $('.goal').click(function () {
+          $('.content1').hide()
+          $('.content3').show()
+
+     })
+ });

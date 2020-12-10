11029d2d2f1795f3fffdb6892bf75502811aede0
Clarisse-3924 <cuwizeyimana3924@gmail.com>
2020-12-10 20:02:31 +0200

    modifying files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,7 +14,7 @@ public class App {
 
         Spark.get("/",(request, response) -> {
             Map<String,Object> model=new HashMap<String, Object>();
-            return new ModelAndView(model,"shoes.hbs");
+            return new ModelAndView(model,"orderform.hbs");
         },new HandlebarsTemplateEngine());
 
 

0cb8bc547c9815685380a95d004574d359dd84ad
brendahuwitonze <uwitonzebrendah@gmail.com>
2020-12-10 23:10:46 +0200

    craete a test

---
+++ src/main/java/ClothesTest.java
@@ -0,0 +1,39 @@
+import static org.junit.Assert.*;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+    public class ClothesTest {
+        @Test
+        public void allClothes ()throws Exception{
+            Clothes testClothes=setUpNewClothes ();
+            assertEquals (true,testClothes instanceof Clothes);
+
+        }
+
+        @Test
+        public void getName ()throws Exception{
+            Clothes testClothes=setUpNewClothes ();
+            assertEquals ("jogging",testClothes.getName ());
+        }
+
+        @Test
+        public void getDesciption ()throws Exception{
+            Clothes testClothes=setUpNewClothes ();
+            assertEquals ("it can be for men or women",testClothes.getDescription ());
+        }
+        @Test
+        public void getprice ()throws Exception{
+            Clothes testClothes=setUpNewClothes ();
+            assertEquals (5000,testClothes.getPrice  ());
+        }
+
+        private Clothes setUpNewClothes(){
+            return new Clothes ("jogging",5000,"it can be for men or women");
+        }
+
+    }
+
+}
